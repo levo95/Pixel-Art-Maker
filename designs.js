cb517504ac45c3238e6e389bc422f6c1aa1ecd46
Lingeshwaran <lingeshwaran1995@gmail.com>
2017-10-15 13:31:31 +0530

    user inputs

---
+++ designs.js
@@ -0,0 +1,18 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+const table = document.getElementById("pixel_canvas");
+
+
+document.getElementById("makeGrid").addEventListener("click", function() {
+  table.innerHTML = "";
+
+  //to get input
+
+  const width = document.getElementById("input_width").value;
+
+  const height = document.getElementById("input_height").value;
+
+}
